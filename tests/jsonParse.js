744b5c6b75793907e5b6cf3506294b6de151111a
Xotic750 <xotic750@gmail.com>
2013-12-29 18:45:49 +0100

    Try to figure out where the throw is coming from

--- tests/jsonParse.js
+++ tests/jsonParse.js
@@ -1,4 +1,4 @@
-/*global require, describe, it */
+/*global require, describe, it, console */
 
 (function () {
     'use strict';
@@ -9,43 +9,53 @@
 
     describe('jsonParse', function () {
         it('should not throw an error in each case', function () {
+            console.log('#1');
             expect(utilx.jsonParse(null)).to.be(null);
+            console.log('#2');
             expect(utilx.jsonParse('-1')).to.be(-1);
+            console.log('#3');
             expect(utilx.jsonParse('0')).to.be(0);
+            console.log('#4');
             expect(utilx.jsonParse('1')).to.be(1);
+            console.log('#5');
             expect(utilx.jsonParse(false)).to.be(false);
+            console.log('#6');
             expect(utilx.jsonParse(true)).to.be(true);
+            console.log('#7');
             expect(utilx.jsonParse('null')).to.be(null);
 
+            console.log('#8');
             expect(function () {
                 utilx.jsonParse();
             }).to.throwException(function (e) {
                 expect(e).to.be.a(SyntaxError);
             });
 
+            console.log('#9');
             expect(function () {
                 utilx.jsonParse(utilx.privateUndefined);
             }).to.throwException(function (e) {
                 expect(e).to.be.a(SyntaxError);
             });
 
+            console.log('#10');
             expect(function () {
                 utilx.jsonParse('');
             }).to.throwException(function (e) {
                 expect(e).to.be.a(SyntaxError);
             });
 
+            console.log('#11');
             expect(function () {
                 utilx.jsonParse('{"A": undefined}');
             }).to.throwException(function (e) {
                 expect(e).to.be.a(SyntaxError);
             });
 
-            /*
+            console.log('#12');
             expect(utilx.jsonParse('{"A":[1,true,false,null,"\\u0000\\b\\n\\f\\r\\t"]}')).to.eql({
                 'A': [1, true, false, null, '\u0000\b\n\f\r\t']
             });
-            */
         });
     });
 }());
