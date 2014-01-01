ef2dce61771040d8de322e788335eb35cc0a4e50
Xotic750 <xotic750@gmail.com>
2014-01-01 17:26:33 +0100

    Further debug IE

--- tests/arraySlice.js
+++ tests/arraySlice.js
@@ -28,27 +28,32 @@
             expect(utilx.arraySlice(someArgs())).to.eql(utilx.arraySlice(arr));
         });
 
-        it('#2: ' + utilx.arraySlice(someArgs(), -1) + ' : ' + utilx.arraySlice(arr, -1), function () {
+        it('#2: ' + utilx.arraySlice(someArgs(), utilx.privateUndefined, utilx.privateUndefined) + ' : ' + utilx.arraySlice(arr, utilx.privateUndefined, utilx.privateUndefined), function () {
+            expect(utilx.arraySlice(someArgs(), utilx.privateUndefined, utilx.privateUndefined))
+                .to.eql(utilx.arraySlice(arr, utilx.privateUndefined, utilx.privateUndefined));
+        });
+
+        it('#3: ' + utilx.arraySlice(someArgs(), -1) + ' : ' + utilx.arraySlice(arr, -1), function () {
             expect(utilx.arraySlice(someArgs(), -1)).to.eql(utilx.arraySlice(arr, -1));
         });
 
-        it('#3: ' + utilx.arraySlice(someArgs(), 0) + ' : ' + utilx.arraySlice(arr, 0), function () {
+        it('#4: ' + utilx.arraySlice(someArgs(), 0) + ' : ' + utilx.arraySlice(arr, 0), function () {
             expect(utilx.arraySlice(someArgs(), 0)).to.eql(utilx.arraySlice(arr, 0));
         });
 
-        it('#4: ' + utilx.arraySlice(someArgs(), 3) + ' : ' + utilx.arraySlice(arr, 3), function () {
+        it('#5: ' + utilx.arraySlice(someArgs(), 3) + ' : ' + utilx.arraySlice(arr, 3), function () {
             expect(utilx.arraySlice(someArgs(), 3)).to.eql(utilx.arraySlice(arr, 3));
         });
 
-        it('#5: ' + utilx.arraySlice(someArgs(), -1, 4) + ' : ' + utilx.arraySlice(arr, -1, 4), function () {
+        it('#6: ' + utilx.arraySlice(someArgs(), -1, 4) + ' : ' + utilx.arraySlice(arr, -1, 4), function () {
             expect(utilx.arraySlice(someArgs(), -1, 4)).to.eql(utilx.arraySlice(arr, -1, 4));
         });
 
-        it('#6: ' + utilx.arraySlice(someArgs(), 0, 4) + ' : ' + utilx.arraySlice(arr, 0, 4), function () {
+        it('#7: ' + utilx.arraySlice(someArgs(), 0, 4) + ' : ' + utilx.arraySlice(arr, 0, 4), function () {
             expect(utilx.arraySlice(someArgs(), 0, 4)).to.eql(utilx.arraySlice(arr, 0, 4));
         });
 
-        it('#7: ' + utilx.arraySlice(someArgs(), 3, 6) + ' : ' + utilx.arraySlice(arr, 3, 6), function () {
+        it('#8: ' + utilx.arraySlice(someArgs(), 3, 6) + ' : ' + utilx.arraySlice(arr, 3, 6), function () {
             expect(utilx.arraySlice(someArgs(), 3, 6)).to.eql(utilx.arraySlice(arr, 3, 6));
         });
     });
