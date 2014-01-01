234e798e7a51f4f8812c6007bd4f446b4a63b846
Xotic750 <xotic750@gmail.com>
2014-01-01 23:08:57 +0100

    Further debug arrayPush

--- tests/arrayPush.js
+++ tests/arrayPush.js
@@ -1,4 +1,4 @@
-/*global require, describe, it */
+/*global require, describe, it, console */
 
 (function () {
     'use strict';
@@ -8,7 +8,6 @@
         expect = required.expect;
 
     describe('arrayPush', function () {
-        //it('should not throw an error in each case', function () {
         var arr = [],
             arrCmp = [],
             arrCmp2 = [
@@ -26,69 +25,35 @@
                 utilx.privateUndefined
             ];
 
-        utilx.arrayPush(arrCmp, utilx.privateUndefined);
-        utilx.arrayPush(arrCmp, null);
-        utilx.arrayPush(arrCmp, -1);
-        utilx.arrayPush(arrCmp, 0);
-        utilx.arrayPush(arrCmp, 1);
-        utilx.arrayPush(arrCmp, false);
-        utilx.arrayPush(arrCmp, true);
-        utilx.arrayPush(arrCmp);
-        utilx.arrayPush(arrCmp, utilx.privateUndefined);
-        utilx.arrayPush(arrCmp, '');
-        utilx.arrayPush(arrCmp, 'abc');
-        utilx.arrayPush(arrCmp, null);
-        utilx.arrayPush(arrCmp, utilx.privateUndefined);
-
-        it('1', function () {
+        it('should not throw an error in each case', function () {
             expect(utilx.arrayPush(arr, null)).to.be(1);
-        });
-
-        it('2', function () {
             expect(utilx.arrayPush(arr, -1)).to.be(2);
-        });
-
-        it('3', function () {
             expect(utilx.arrayPush(arr, 0)).to.be(3);
-        });
-
-        it('4', function () {
             expect(utilx.arrayPush(arr, 1)).to.be(4);
-        });
-
-        it('5', function () {
             expect(utilx.arrayPush(arr, false)).to.be(5);
-        });
-
-        it('6', function () {
             expect(utilx.arrayPush(arr, true)).to.be(6);
-        });
-
-        it('7', function () {
             expect(utilx.arrayPush(arr)).to.be(6);
-        });
-
-        it('8', function () {
             expect(utilx.arrayPush(arr, utilx.privateUndefined)).to.be(7);
-        });
-
-        it('9', function () {
             expect(utilx.arrayPush(arr, '')).to.be(8);
-        });
-
-        it('10', function () {
             expect(utilx.arrayPush(arr, 'abc')).to.be(9);
         });
 
-        it('11: ' + arrCmp.toString() + ' : ' + arrCmp2.toString(), function () {
-            expect(utilx.deepEqual(arrCmp, arrCmp2)).to.be.ok();
-        });
-
-        it('12: ' + arrCmp.toString() + ' : ' + arrCmp2.toString(), function () {
-            expect(arrCmp.toString()).to.be(arrCmp2.toString());
-        });
-
-        it('13: ' + arrCmp.toString() + ' : ' + arrCmp2.toString(), function () {
+        it('11', function () {
+            utilx.arrayPush(arrCmp, utilx.privateUndefined);
+            utilx.arrayPush(arrCmp, null);
+            utilx.arrayPush(arrCmp, -1);
+            utilx.arrayPush(arrCmp, 0);
+            utilx.arrayPush(arrCmp, 1);
+            utilx.arrayPush(arrCmp, false);
+            utilx.arrayPush(arrCmp, true);
+            utilx.arrayPush(arrCmp);
+            utilx.arrayPush(arrCmp, utilx.privateUndefined);
+            utilx.arrayPush(arrCmp, '');
+            utilx.arrayPush(arrCmp, 'abc');
+            utilx.arrayPush(arrCmp, null);
+            utilx.arrayPush(arrCmp, utilx.privateUndefined);
+            console.log('# ' + utilx.deepEqual(arrCmp, arrCmp2));
+            console.log('# ' + arrCmp.toString() + ' : ' + arrCmp2.toString());
             expect(arrCmp).to.eql(arrCmp2);
         });
     });
