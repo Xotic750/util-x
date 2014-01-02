324afe105f0bb265fbab53c27a4e7b2034fa8e70
Xotic750 <xotic750@gmail.com>
2014-01-02 03:38:45 +0100

    Try a custom push and rearrange length

--- tests/arrayPush.js
+++ tests/arrayPush.js
@@ -1,4 +1,4 @@
-/*global require, describe, it */
+/*global require, describe, it, console */
 
 (function () {
     'use strict';
@@ -6,6 +6,20 @@
     var required = require('../scripts/'),
         utilx = required.utilx,
         expect = required.expect;
+
+    function push(array) {
+        /*jslint bitwise: true */
+        var n = utilx.toUint32(array.length),
+            i;
+
+        for (i = 1; i < arguments.length; n = utilx.toUint32(n + 1), i += 1) {
+            array[n] = arguments[i];
+        }
+
+        array.length = n;
+
+        return n;
+    }
 
     describe('arrayPush', function () {
         it('should not throw an error in each case', function () {
@@ -23,7 +37,7 @@
             expect(utilx.arrayPush(arr, 'abc')).to.be(9);
         });
 
-        it('8', function () {
+        it('7', function () {
             var arrCmp = [
                     utilx.privateUndefined,
                     null,
@@ -63,6 +77,45 @@
             //expect(arrCmp).to.eql(arrCmp2);
         });
 
+        it('8', function () {
+            var arrCmp = [],
+                arrCmp2 = [
+                    utilx.privateUndefined,
+                    null,
+                    -1,
+                    0,
+                    1,
+                    false,
+                    true,
+                    utilx.privateUndefined,
+                    '',
+                    'abc',
+                    null,
+                    utilx.privateUndefined
+                ],
+                i;
+
+            push(arrCmp, utilx.privateUndefined);
+            push(arrCmp, null);
+            push(arrCmp, -1);
+            push(arrCmp, 0);
+            push(arrCmp, 1);
+            push(arrCmp, false);
+            push(arrCmp, true);
+            push(arrCmp, utilx.privateUndefined);
+            push(arrCmp, '');
+            push(arrCmp, 'abc');
+            push(arrCmp, null);
+            push(arrCmp, utilx.privateUndefined);
+            expect(arrCmp.length).to.be(arrCmp2.length);
+            for (i = 0; i < arrCmp2.length; i += 1) {
+                expect(arrCmp[i]).to.be(arrCmp2[i]);
+            }
+
+            expect(utilx.deepEqual(arrCmp, arrCmp2)).to.be(true);
+            //expect(arrCmp).to.eql(arrCmp2);
+        });
+
         it('9', function () {
             var arrCmp = [],
                 arrCmp2 = [
@@ -81,7 +134,6 @@
                 ],
                 i;
 
-            arrCmp.length = 12;
             arrCmp[0] = utilx.privateUndefined;
             arrCmp[1] = null;
             arrCmp[2] = -1;
@@ -94,6 +146,7 @@
             arrCmp[9] = 'abc';
             arrCmp[10] = null;
             arrCmp[11] = utilx.privateUndefined;
+            arrCmp.length = 12;
             expect(arrCmp.length).to.be(arrCmp2.length);
             for (i = 0; i < arrCmp2.length; i += 1) {
                 expect(arrCmp[i]).to.be(arrCmp2[i]);
@@ -136,6 +189,7 @@
             arrCmp.push('abc');
             arrCmp.push(null);
             arrCmp.push(utilx.privateUndefined);
+            arrCmp.length = 12;
             expect(arrCmp.length).to.be(arrCmp2.length);
             for (i = 0; i < arrCmp2.length; i += 1) {
                 expect(arrCmp[i]).to.be(arrCmp2[i]);
@@ -178,6 +232,7 @@
             utilx.arrayPush(arrCmp, 'abc');
             utilx.arrayPush(arrCmp, null);
             utilx.arrayPush(arrCmp, utilx.privateUndefined);
+            arrCmp.length = 12;
             expect(arrCmp.length).to.be(arrCmp2.length);
             for (i = 0; i < arrCmp2.length; i += 1) {
                 expect(arrCmp[i]).to.be(arrCmp2[i]);
