b69ed12fce9876429bb6a21eb1d182d2653fe8c5
Xotic750 <xotic750@gmail.com>
2014-01-02 01:49:28 +0100

    Check the lengths and each element

--- tests/arrayPush.js
+++ tests/arrayPush.js
@@ -1,4 +1,4 @@
-/*global require, describe, it, console */
+/*global require, describe, it */
 
 (function () {
     'use strict';
@@ -8,24 +8,9 @@
         expect = required.expect;
 
     describe('arrayPush', function () {
-        var arr = [],
-            arrCmp = [],
-            arrCmp2 = [
-                utilx.privateUndefined,
-                null,
-                -1,
-                0,
-                1,
-                false,
-                true,
-                utilx.privateUndefined,
-                '',
-                'abc',
-                null,
-                utilx.privateUndefined
-            ];
-
         it('should not throw an error in each case', function () {
+            var arr = [];
+
             expect(utilx.arrayPush(arr, null)).to.be(1);
             expect(utilx.arrayPush(arr, -1)).to.be(2);
             expect(utilx.arrayPush(arr, 0)).to.be(3);
@@ -39,6 +24,23 @@
         });
 
         it('11', function () {
+            var arrCmp = [],
+                arrCmp2 = [
+                    utilx.privateUndefined,
+                    null,
+                    -1,
+                    0,
+                    1,
+                    false,
+                    true,
+                    utilx.privateUndefined,
+                    '',
+                    'abc',
+                    null,
+                    utilx.privateUndefined
+                ],
+                i;
+
             utilx.arrayPush(arrCmp, utilx.privateUndefined);
             utilx.arrayPush(arrCmp, null);
             utilx.arrayPush(arrCmp, -1);
@@ -47,17 +49,19 @@
             utilx.arrayPush(arrCmp, false);
             utilx.arrayPush(arrCmp, true);
             utilx.arrayPush(arrCmp);
+            utilx.arrayPush(arrCmp);
+            utilx.arrayPush(arrCmp);
             utilx.arrayPush(arrCmp, utilx.privateUndefined);
             utilx.arrayPush(arrCmp, '');
             utilx.arrayPush(arrCmp, 'abc');
             utilx.arrayPush(arrCmp, null);
             utilx.arrayPush(arrCmp, utilx.privateUndefined);
-            console.log('# ' + arrCmp.length + ' : ' + arrCmp2.length);
-            for (var i = 0; i < arrCmp2.length; i += 1) {
-                console.log('# ' + arrCmp[i] + ' : ' + arrCmp2[i]);
+            expect(arrCmp.length).to.be(arrCmp2.length);
+            for (i = 0; i < arrCmp2.length; i += 1) {
+                expect(arrCmp[i]).to.be(arrCmp2[i]);
             }
 
-            expect(arrCmp).to.eql(arrCmp2);
+            //expect(arrCmp).to.eql(arrCmp2);
         });
     });
 }());
