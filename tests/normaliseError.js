2c8c1403d329edfad044486c47866f1d44528d78
Xotic750 <xotic750@gmail.com>
2014-01-06 04:41:57 +0100

    Allow IE Error normalisation to be turned on and off

--- tests/normaliseError.js
+++ tests/normaliseError.js
@@ -11,13 +11,17 @@
         it('should not throw an error in each case', function () {
             var message = 'We want a normalised toString!';
 
+            expect(!utilx.normaliseErrorIEToString.off()).to.be.ok();
+            expect(!utilx.normaliseErrorIEToString.state()).to.be.ok();
+
             try {
                 throw new Error(message);
             } catch (e) {
                 if (utilx.strictEqual(e.message, message) &&
                         utilx.strictEqual(e.toString(), '[object Error]')) {
 
-                    expect(utilx.normaliseErrorIEToString()).to.be.ok();
+                    expect(utilx.normaliseErrorIEToString.on()).to.be.ok();
+                    expect(utilx.normaliseErrorIEToString.state()).to.be.ok();
                 }
             }
 
@@ -27,6 +31,9 @@
                 expect(e).to.be.a(Error);
                 expect(e.toString()).to.match(new RegExp('^Error: ' + message));
             });
+
+            expect(!utilx.normaliseErrorIEToString.off()).to.be.ok();
+            expect(!utilx.normaliseErrorIEToString.state()).to.be.ok();
         });
     });
 }());
