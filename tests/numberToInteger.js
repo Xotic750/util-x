85262376a91e8237576dd42a50c6ef91df44e6ba
Xotic750 <xotic750@gmail.com>
2014-01-10 05:06:03 +0100

    Add some more tests to narrow doen the FF3 issue

--- tests/numberToInteger.js
+++ tests/numberToInteger.js
@@ -46,6 +46,67 @@
             expect(utilx.numberToInteger(' 10.123')).to.be(10);
             expect(utilx.numberToInteger('10.123 ')).to.be(10);
             expect(utilx.numberToInteger(' 10.123 ')).to.be(10);
+
+            expect(utilx.numberToInteger('-1')).to.be(-1);
+            expect(utilx.numberToInteger('0')).to.be(0);
+            expect(utilx.numberToInteger('1')).to.be(1);
+            expect(utilx.numberToInteger('-1.')).to.be(-1);
+            expect(utilx.numberToInteger('0.')).to.be(0);
+            expect(utilx.numberToInteger('1.')).to.be(1);
+            /*jshint -W047 */
+            expect(utilx.numberToInteger(-1.)).to.be(-1);
+            expect(utilx.numberToInteger(0.)).to.be(0);
+            expect(utilx.numberToInteger(1.)).to.be(1);
+            /*jshint +W047 */
+            expect(utilx.numberToInteger('-1.1')).to.be(-1);
+            expect(utilx.numberToInteger('0.1')).to.be(0);
+            expect(utilx.numberToInteger('1.1')).to.be(1);
+            expect(utilx.isNumber(utilx.numberToInteger(new Date(2013, 11, 11)))).to.be.ok();
+            expect(utilx.numberIsNaN(utilx.numberToInteger(new Date(2013, 11, 11)))).to.not.be.ok();
+            expect(utilx.numberToInteger('NaN')).to.be(0);
+            expect(utilx.numberToInteger('Infinity')).to.be(Infinity);
+            expect(utilx.numberToInteger('-Infinity')).to.be(-Infinity);
+            expect(utilx.numberToInteger([])).to.be(0);
+            expect(utilx.numberToInteger([1])).to.be(1);
+            expect(utilx.numberToInteger([1.1])).to.be(1);
+            /*jshint -W047 */
+            expect(utilx.numberToInteger([1.])).to.be(1);
+            /*jshint +W047 */
+            expect(utilx.numberToInteger([''])).to.be(0);
+            expect(utilx.numberToInteger(['1'])).to.be(1);
+            expect(utilx.numberToInteger(['1.1'])).to.be(1);
+            expect(utilx.numberToInteger({})).to.be(0);
+            expect(utilx.numberToInteger({valueOf: ''})).to.be(0);
+            expect(utilx.numberToInteger({valueOf: '1'})).to.be(0);
+            expect(utilx.numberToInteger({valueOf: 1})).to.be(0);
+            expect(utilx.numberToInteger({valueOf: 1.1})).to.be(0);
+            /*jshint -W047 */
+            expect(utilx.numberToInteger({valueOf: 1.})).to.be(0);
+            /*jshint +W047 */
+
+            expect(function () {
+                utilx.numberToInteger({toString: ''});
+            }).to.throwException(TypeError);
+
+            expect(function () {
+                utilx.numberToInteger({toString: '1'});
+            }).to.throwException(TypeError);
+
+            expect(function () {
+                utilx.numberToInteger({toString: 1});
+            }).to.throwException(TypeError);
+
+            expect(function () {
+                utilx.numberToInteger({toString: 1.1});
+            }).to.throwException(TypeError);
+
+            /*jshint -W047 */
+            expect(function () {
+                utilx.numberToInteger({toString: 1.});
+            }).to.throwException(TypeError);
+            /*jshint +W047 */
+
+            expect(utilx.numberToInteger(function () { return 1; })).to.be(0);
         });
     });
 }());
--- tests/toNumber.js
+++ tests/toNumber.js
@@ -12,13 +12,65 @@
             expect(utilx.toNumber('-1')).to.be(-1);
             expect(utilx.toNumber('0')).to.be(0);
             expect(utilx.toNumber('1')).to.be(1);
-            expect(utilx.toNumber('Infinity')).to.be(Infinity);
-            expect(utilx.toNumber('-Infinity')).to.be(-Infinity);
+            expect(utilx.toNumber('-1.')).to.be(-1);
+            expect(utilx.toNumber('0.')).to.be(0);
+            expect(utilx.toNumber('1.')).to.be(1);
+            /*jshint -W047 */
+            expect(utilx.toNumber(-1.)).to.be(-1);
+            expect(utilx.toNumber(0.)).to.be(0);
+            expect(utilx.toNumber(1.)).to.be(1);
+            /*jshint +W047 */
+            expect(utilx.toNumber('-1.1')).to.be(-1.1);
+            expect(utilx.toNumber('0.1')).to.be(0.1);
+            expect(utilx.toNumber('1.1')).to.be(1.1);
+            expect(utilx.toNumber(Infinity)).to.be(Infinity);
+            expect(utilx.toNumber(-Infinity)).to.be(-Infinity);
             expect(utilx.isNumber(utilx.toNumber(new Date(2013, 11, 11)))).to.be.ok();
             expect(utilx.numberIsNaN(utilx.toNumber(new Date(2013, 11, 11)))).to.not.be.ok();
             expect(utilx.numberIsNaN(utilx.toNumber('NaN'))).to.be.ok();
             expect(utilx.toNumber('Infinity')).to.be(Infinity);
             expect(utilx.toNumber('-Infinity')).to.be(-Infinity);
+            expect(utilx.toNumber([])).to.be(0);
+            expect(utilx.toNumber([1])).to.be(1);
+            expect(utilx.toNumber([1.1])).to.be(1.1);
+            /*jshint -W047 */
+            expect(utilx.toNumber([1.])).to.be(1);
+            /*jshint +W047 */
+            expect(utilx.toNumber([''])).to.be(0);
+            expect(utilx.toNumber(['1'])).to.be(1);
+            expect(utilx.toNumber(['1.1'])).to.be(1.1);
+            expect(utilx.numberIsNaN(utilx.toNumber({}))).to.be.ok();
+            expect(utilx.numberIsNaN(utilx.toNumber({valueOf: ''}))).to.be.ok();
+            expect(utilx.numberIsNaN(utilx.toNumber({valueOf: '1'}))).to.be.ok();
+            expect(utilx.numberIsNaN(utilx.toNumber({valueOf: 1}))).to.be.ok();
+            expect(utilx.numberIsNaN(utilx.toNumber({valueOf: 1.1}))).to.be.ok();
+            /*jshint -W047 */
+            expect(utilx.numberIsNaN(utilx.toNumber({valueOf: 1.}))).to.be.ok();
+            /*jshint +W047 */
+
+            expect(function () {
+                utilx.toNumber({toString: ''});
+            }).to.throwException(TypeError);
+
+            expect(function () {
+                utilx.toNumber({toString: '1'});
+            }).to.throwException(TypeError);
+
+            expect(function () {
+                utilx.toNumber({toString: 1});
+            }).to.throwException(TypeError);
+
+            expect(function () {
+                utilx.toNumber({toString: 1.1});
+            }).to.throwException(TypeError);
+
+            /*jshint -W047 */
+            expect(function () {
+                utilx.toNumber({toString: 1.});
+            }).to.throwException(TypeError);
+            /*jshint +W047 */
+
+            expect(utilx.numberIsNaN(utilx.toNumber(function () { return 1; }))).to.be.ok();
         });
     });
 }());
