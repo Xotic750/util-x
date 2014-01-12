65abd97984ac86c815d31f6fcd97a976a23f8bc0
Xotic750 <xotic750@gmail.com>
2014-01-12 17:16:58 +0100

    Split tests into suites, issue is probably a shim from browserify

--- tests/isEmptyObject.js
+++ tests/isEmptyObject.js
@@ -7,8 +7,22 @@
         utilx = required.utilx,
         expect = required.expect;
 
+    function F() {
+        return;
+    }
+
+    F.foo = utilx.noop;
+
+    expect(utilx.isEmptyObject(F)).to.not.be.ok();
+
+    function X() {
+        return;
+    }
+
+    X.prototype.foo = utilx.noop;
+
     describe('isEmptyObject', function () {
-        it('should not throw an error in each case', function () {
+        it('should throw a TypeError in each case', function () {
             expect(function () {
                 utilx.isEmptyObject();
             }).to.throwException(function (e) {
@@ -44,31 +58,22 @@
             }).to.throwException(function (e) {
                 expect(e).to.be.a(TypeError);
             });
+        });
 
+        it('should be ok in each case', function () {
             expect(utilx.isEmptyObject({})).to.be.ok();
             expect(utilx.isEmptyObject([])).to.be.ok();
             expect(utilx.isEmptyObject(utilx.noop)).to.be.ok();
             expect(utilx.isEmptyObject(new Date())).to.be.ok();
             expect(utilx.isEmptyObject(new Error('x'))).to.be.ok();
+            expect(utilx.isEmptyObject(X)).to.be.ok();
+        });
+
+        it('should not be ok in each case', function () {
             expect(utilx.isEmptyObject({string: utilx.noop})).to.not.be.ok();
             expect(utilx.isEmptyObject({valueOf: utilx.noop})).to.not.be.ok();
             expect(utilx.isEmptyObject([1])).to.not.be.ok();
-
-            function F() {
-                return;
-            }
-
-            F.foo = utilx.noop;
-
             expect(utilx.isEmptyObject(F)).to.not.be.ok();
-
-            function X() {
-                return;
-            }
-
-            X.prototype.foo = utilx.noop;
-
-            expect(utilx.isEmptyObject(X)).to.be.ok();
             expect(utilx.isEmptyObject(X.prototype)).to.not.be.ok();
         });
     });
