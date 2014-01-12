a3031d85423e15d2d2de4e6031b7962b3978f897
Xotic750 <xotic750@gmail.com>
2014-01-12 19:42:56 +0100

    Added some gotcha nots and removed tests that are not repeatable across browsers

--- tests/isEmptyObject.js
+++ tests/isEmptyObject.js
@@ -7,20 +7,6 @@
         utilx = required.utilx,
         expect = required.expect;
 
-    function F() {
-        return;
-    }
-
-    F.foo = utilx.noop;
-
-    expect(utilx.isEmptyObject(F)).to.not.be.ok();
-
-    function X() {
-        return;
-    }
-
-    X.prototype.foo = utilx.noop;
-
     describe('isEmptyObject', function () {
         it('should throw a TypeError in each case', function () {
             expect(function () {
@@ -61,28 +47,14 @@
         });
 
         it('should be ok in each case', function () {
-            console.log('# OBJECT: ' + utilx.objectKeys({}));
             expect(utilx.isEmptyObject({})).to.be.ok();
-            console.log('# ARRAY: ' + utilx.objectKeys([]));
             expect(utilx.isEmptyObject([])).to.be.ok();
-            console.log('# FUNCTION: ' + utilx.objectKeys(utilx.noop));
-            expect(utilx.isEmptyObject(utilx.noop)).to.be.ok();
-            console.log('# DATE: ' + utilx.objectKeys(new Date()));
-            expect(utilx.isEmptyObject(new Date())).to.be.ok();
-            console.log('# ERROR1: ' + utilx.objectKeys(new Error('x')));
-            expect(utilx.isEmptyObject(new Error('x'))).to.be.ok();
-            console.log('# FUNCTION: ' + utilx.objectKeys(new Error('x')));
-            expect(utilx.isEmptyObject(X)).to.be.ok();
-            console.log('# ERROR2: ' + utilx.objectKeys(Error));
-            expect(utilx.isEmptyObject(Error)).to.be.ok();
         });
 
         it('should not be ok in each case', function () {
             expect(utilx.isEmptyObject({string: utilx.noop})).to.not.be.ok();
             expect(utilx.isEmptyObject({valueOf: utilx.noop})).to.not.be.ok();
             expect(utilx.isEmptyObject([1])).to.not.be.ok();
-            expect(utilx.isEmptyObject(F)).to.not.be.ok();
-            expect(utilx.isEmptyObject(X.prototype)).to.not.be.ok();
         });
     });
 }());
