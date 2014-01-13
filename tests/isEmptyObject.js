b067c5dc1e77c6920be296087f2e5a84dff69c3e
Xotic750 <xotic750@gmail.com>
2014-01-13 01:43:56 +0100

    Add some logging to determing repeatability of propertyIsEnumerable

--- tests/isEmptyObject.js
+++ tests/isEmptyObject.js
@@ -7,6 +7,20 @@
         utilx = required.utilx,
         expect = required.expect;
 
+    function F() {
+        return;
+    }
+
+    F.foo = utilx.noop;
+
+    expect(utilx.isEmptyObject(F)).to.not.be.ok();
+
+    function X() {
+        return;
+    }
+
+    X.prototype.foo = utilx.noop;
+
     describe('isEmptyObject', function () {
         it('should throw a TypeError in each case', function () {
             expect(function () {
@@ -49,12 +63,25 @@
         it('should be ok in each case', function () {
             expect(utilx.isEmptyObject({})).to.be.ok();
             expect(utilx.isEmptyObject([])).to.be.ok();
+            console.log('# FUNCTION: ' + utilx.objectKeys(utilx.noop));
+            expect(utilx.isEmptyObject(utilx.noop)).to.be.ok();
+            expect(utilx.isEmptyObject(new Date())).to.be.ok();
+            console.log('# ERROR: ' + utilx.objectKeys(new Error('x')));
+            expect(utilx.isEmptyObject(new Error('x'))).to.be.ok();
+            console.log('# FUNCTION: ' + utilx.objectKeys(new Error('x')));
+            expect(utilx.isEmptyObject(X)).to.be.ok();
+            expect(utilx.isEmptyObject(Error)).to.be.ok();
+            expect(utilx.isEmptyObject(TypeError)).to.be.ok();
+            expect(utilx.isEmptyObject(SyntaxError)).to.be.ok();
+            expect(utilx.isEmptyObject(RangeError)).to.be.ok();
         });
 
         it('should not be ok in each case', function () {
             expect(utilx.isEmptyObject({string: utilx.noop})).to.not.be.ok();
             expect(utilx.isEmptyObject({valueOf: utilx.noop})).to.not.be.ok();
             expect(utilx.isEmptyObject([1])).to.not.be.ok();
+            expect(utilx.isEmptyObject(F)).to.not.be.ok();
+            expect(utilx.isEmptyObject(X.prototype)).to.not.be.ok();
         });
     });
 }());
---
+++ tests/objectPropertyIsEnumerable.js
@@ -0,0 +1,55 @@
+/*global require, describe, it */
+
+(function () {
+    'use strict';
+
+    var required = require('../scripts/'),
+        utilx = required.utilx,
+        expect = required.expect;
+
+    describe('objectPropertyIsEnumerable', function () {
+        /*jshint -W001 */
+        var obj = {
+                'toString': utilx.noop,
+                'toLocaleString': utilx.noop,
+                'valueOf': utilx.noop,
+                'hasOwnProperty': utilx.noop,
+                'isPrototypeOf': utilx.noop,
+                'propertyIsEnumerable': utilx.noop,
+                'constructor': utilx.noop
+            },
+            obj2 = {};
+        /*jshint +W001 */
+
+        it('should not throw an error in each case', function () {
+            expect(utilx.objectPropertyIsEnumerable(obj, 'toString')).to.be.ok();
+            expect(utilx.objectPropertyIsEnumerable(obj, 'toLocaleString')).to.be.ok();
+            expect(utilx.objectPropertyIsEnumerable(obj, 'valueOf')).to.be.ok();
+            expect(utilx.objectPropertyIsEnumerable(obj, 'hasOwnProperty')).to.be.ok();
+            expect(utilx.objectPropertyIsEnumerable(obj, 'isPrototypeOf')).to.be.ok();
+            expect(utilx.objectPropertyIsEnumerable(obj, 'propertyIsEnumerable')).to.be.ok();
+            expect(utilx.objectPropertyIsEnumerable(obj, 'constructor')).to.be.ok();
+            expect(utilx.objectPropertyIsEnumerable(obj, 'foo')).to.not.be.ok();
+            expect(utilx.objectPropertyIsEnumerable(obj, 'bar')).to.not.be.ok();
+            expect(utilx.objectPropertyIsEnumerable(obj, 'fuz')).to.not.be.ok();
+
+            expect(utilx.objectPropertyIsEnumerable(obj2, 'toString')).to.not.be.ok();
+            expect(utilx.objectPropertyIsEnumerable(obj2, 'toLocaleString')).to.not.be.ok();
+            expect(utilx.objectPropertyIsEnumerable(obj2, 'valueOf')).to.not.be.ok();
+            expect(utilx.objectPropertyIsEnumerable(obj2, 'hasOwnProperty')).to.not.be.ok();
+            expect(utilx.objectPropertyIsEnumerable(obj2, 'isPrototypeOf')).to.not.be.ok();
+            expect(utilx.objectPropertyIsEnumerable(obj2, 'propertyIsEnumerable')).to.not.be.ok();
+            expect(utilx.objectPropertyIsEnumerable(obj2, 'constructor')).to.not.be.ok();
+            expect(utilx.objectPropertyIsEnumerable(obj2, 'foo')).to.not.be.ok();
+            expect(utilx.objectPropertyIsEnumerable(obj2, 'bar')).to.not.be.ok();
+            expect(utilx.objectPropertyIsEnumerable(obj2, 'fuz')).to.not.be.ok();
+
+            expect(utilx.objectPropertyIsEnumerable({
+                toString: utilx.privateUndefined
+            }, 'toString')).to.be.ok();
+
+            expect(utilx.objectPropertyIsEnumerable({}, 'toString')).to.not.be.ok();
+            expect(utilx.objectPropertyIsEnumerable({}, 'bar')).to.not.be.ok();
+        });
+    });
+}());
