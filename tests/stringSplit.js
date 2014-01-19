de310191628486dba1016008705545d1ab7c6bd4
Xotic750 <xotic750@gmail.com>
2014-01-19 18:05:36 +0100

    Some stringSplit detection for fixes, add some logging

--- tests/stringSplit.js
+++ tests/stringSplit.js
@@ -11,25 +11,31 @@
         var test,
             msg;
 
-        it('should not throw an error in each case', function () {
+        it('should throw if no arguments', function () {
             expect(function () {
                 utilx.stringSplit();
             }).to.throwException(function (e) {
                 expect(e).to.be.a(TypeError);
             });
+        });
 
+        it('should throw if argument is undefined', function () {
             expect(function () {
                 utilx.stringSplit(utilx.privateUndefined);
             }).to.throwException(function (e) {
                 expect(e).to.be.a(TypeError);
             });
+        });
 
+        it('should throw if argument is null', function () {
             expect(function () {
                 utilx.stringSplit(null);
             }).to.throwException(function (e) {
                 expect(e).to.be.a(TypeError);
             });
+        });
 
+        it('should not throw on basic tests', function () {
             expect(utilx.stringSplit('abcdef', '')).to.eql(['a', 'b', 'c', 'd', 'e', 'f']);
             expect(utilx.stringSplit('abcdefabcdefabcdef', 'c')).to.eql(['ab', 'defab', 'defab', 'def']);
             expect(utilx.stringSplit('abcdefabcdefabcdef', new RegExp('c'))).to.eql(['ab', 'defab', 'defab', 'def']);
@@ -103,13 +109,13 @@
             expect(utilx.stringSplit(txt, /(?:ab)*?/)).to.eql(['a', 'b']);
         });
 
-        it('stringSplit(test\'\') results in [\'t\', \'e\', \'s\', \'t\']', function () {
+        it('stringSplit(\'test\', \'\') results in [\'t\', \'e\', \'s\', \'t\']', function () {
             var txt = 'test';
 
             expect(utilx.stringSplit(txt, '')).to.eql(['t', 'e', 's', 't']);
         });
 
-        it('stringSplit(test) results in [\'test\']', function () {
+        it('stringSplit(\'test\', ) results in [\'test\']', function () {
             var txt = 'test';
 
             expect(utilx.stringSplit(txt)).to.eql(['test']);
@@ -121,49 +127,49 @@
             expect(utilx.stringSplit(txt, 1)).to.eql(['', '', '', '']);
         });
 
-        it('stringSplit(test/(?:)/, 2) results in [\'t\', \'e\']', function () {
+        it('stringSplit(\'test\', /(?:)/, 2) results in [\'t\', \'e\']', function () {
             var txt = 'test';
 
             expect(utilx.stringSplit(txt, /(?:)/, 2)).to.eql(['t', 'e']);
         });
 
-        it('stringSplit(test/(?:)/, -1) results in [\'t\', \'e\', \'s\', \'t\']', function () {
+        it('stringSplit(\'test\', /(?:)/, -1) results in [\'t\', \'e\', \'s\', \'t\']', function () {
             var txt = 'test';
 
             expect(utilx.stringSplit(txt, /(?:)/, -1)).to.eql(['t', 'e', 's', 't']);
         });
 
-        it('stringSplit(test/(?:)/, undefined) results in [\'t\', \'e\', \'s\', \'t\']', function () {
+        it('stringSplit(\'test\', /(?:)/, undefined) results in [\'t\', \'e\', \'s\', \'t\']', function () {
             var txt = 'test';
 
             expect(utilx.stringSplit(txt, /(?:)/, undefined)).to.eql(['t', 'e', 's', 't']);
         });
 
-        it('stringSplit(test/(?:)/, null) results in []', function () {
+        it('stringSplit(\'test\', /(?:)/, null) results in []', function () {
             var txt = 'test';
 
             expect(utilx.stringSplit(txt, /(?:)/, null)).to.eql([]);
         });
 
-        it('stringSplit(test/(?:)/, NaN) results in []', function () {
+        it('stringSplit(\'test\', /(?:)/, NaN) results in []', function () {
             var txt = 'test';
 
             expect(utilx.stringSplit(txt, /(?:)/, NaN)).to.eql([]);
         });
 
-        it('stringSplit(test/(?:)/, true) results in [\'t\']', function () {
+        it('stringSplit(\'test\', /(?:)/, true) results in [\'t\']', function () {
             var txt = 'test';
 
             expect(utilx.stringSplit(txt, /(?:)/, true)).to.eql(['t']);
         });
 
-        it('stringSplit(test/(?:)/, \'2\') results in [\'t\', \'e\']', function () {
+        it('stringSplit(\'test\', /(?:)/, \'2\') results in [\'t\', \'e\']', function () {
             var txt = 'test';
 
             expect(utilx.stringSplit(txt, /(?:)/, '2')).to.eql(['t', 'e']);
         });
 
-        it('stringSplit(test/(?:)/, \'two\') results in []', function () {
+        it('stringSplit(\'test\', /(?:)/, \'two\') results in []', function () {
             var txt = 'test';
 
             expect(utilx.stringSplit(txt, /(?:)/, 'two')).to.eql([]);
@@ -302,7 +308,7 @@
                 .to.eql(['A', undefined, 'B', 'bold', '/', 'B', 'and', undefined, 'CODE', 'coded', '/', 'CODE', '']);
         });
 
-        it('stringSplit(test/(s)*/) results in [\'t\', undefined, \'e\', \'s\', \'t\']', function () {
+        it('stringSplit(\'test\', /(s)*/) results in [\'t\', undefined, \'e\', \'s\', \'t\']', function () {
             var txt = 'tesst';
 
             expect(utilx.stringSplit(txt, /(s)*/)).to.eql(['t', undefined, 'e', 's', 't']);
@@ -317,7 +323,7 @@
                 .to.eql(['t', undefined, 'e', undefined, 's', undefined, 's', undefined, 't']);
         });
 
-        it('stringSplit(test/(s*)/) results in [\'t\', \'\', \'e\', \'ss\', \'t\']', function () {
+        it('stringSplit(\'test\', /(s*)/) results in [\'t\', \'\', \'e\', \'ss\', \'t\']', function () {
             var txt = 'tesst';
 
             expect(utilx.stringSplit(txt, /(s*)/)).to.eql(['t', '', 'e', 'ss', 't']);
@@ -331,55 +337,55 @@
             expect(utilx.stringSplit(txt, /(s*?)/)).to.eql(['t', '', 'e', '', 's', '', 's', '', 't']);
         });
 
-        it('stringSplit(test/(?:s)*/) results in [\'t\', \'e\', \'t\']', function () {
+        it('stringSplit(\'test\', /(?:s)*/) results in [\'t\', \'e\', \'t\']', function () {
             var txt = 'tesst';
 
             expect(utilx.stringSplit(txt, /(?:s)*/)).to.eql(['t', 'e', 't']);
         });
 
-        it('stringSplit(test/(?=s+)/) results in [\'te\', \'s\', \'st\']', function () {
+        it('stringSplit(\'test\', /(?=s+)/) results in [\'te\', \'s\', \'st\']', function () {
             var txt = 'tesst';
 
             expect(utilx.stringSplit(txt, /(?=s+)/)).to.eql(['te', 's', 'st']);
         });
 
-        it('stringSplit(test\'t\') results in [\'\', \'es\', \'\']', function () {
+        it('stringSplit(\'test\', \'t\') results in [\'\', \'es\', \'\']', function () {
             var txt = 'test';
 
             expect(utilx.stringSplit(txt, 't')).to.eql(['', 'es', '']);
         });
 
-        it('stringSplit(test\'es\') results in [\'t\', \'t\']', function () {
+        it('stringSplit(\'test\', \'es\') results in [\'t\', \'t\']', function () {
             var txt = 'test';
 
             expect(utilx.stringSplit(txt, 'es')).to.eql(['t', 't']);
         });
 
-        it('stringSplit(test/t/) results in [\'\', \'es\', \'\']', function () {
+        it('stringSplit(\'test\', /t/) results in [\'\', \'es\', \'\']', function () {
             var txt = 'test';
 
             expect(utilx.stringSplit(txt, /t/)).to.eql(['', 'es', '']);
         });
 
-        it('stringSplit(test/es/) results in [\'t\', \'t\']', function () {
+        it('stringSplit(\'test\', /es/) results in [\'t\', \'t\']', function () {
             var txt = 'test';
 
             expect(utilx.stringSplit(txt, /es/)).to.eql(['t', 't']);
         });
 
-        it('stringSplit(test/(t)/) results in [\'\', \'t\', \'es\', \'t\', \'\']', function () {
+        it('stringSplit(\'test\', /(t)/) results in [\'\', \'t\', \'es\', \'t\', \'\']', function () {
             var txt = 'test';
 
             expect(utilx.stringSplit(txt, /(t)/)).to.eql(['', 't', 'es', 't', '']);
         });
 
-        it('stringSplit(test/(es)/) results in [\'t\', \'es\', \'t\']', function () {
+        it('stringSplit(\'test\', /(es)/) results in [\'t\', \'es\', \'t\']', function () {
             var txt = 'test';
 
             expect(utilx.stringSplit(txt, /(es)/)).to.eql(['t', 'es', 't']);
         });
 
-        it('stringSplit(test/(t)(e)(s)(t)/) results in [\'\', \'t\', \'e\', \'s\', \'t\', \'\']', function () {
+        it('stringSplit(\'test\', /(t)(e)(s)(t)/) results in [\'\', \'t\', \'e\', \'s\', \'t\', \'\']', function () {
             var txt = 'test';
 
             expect(utilx.stringSplit(txt, /(t)(e)(s)(t)/)).to.eql(['', 't', 'e', 's', 't', '']);
