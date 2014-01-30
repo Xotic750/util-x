8cf53fa716e1188ee6f89577bab4fde0eb5a2dda
Xotic750 <xotic750@gmail.com>
2014-01-30 01:24:56 +0100

    Update arraySlice test

--- tests/arraySlice.js
+++ tests/arraySlice.js
@@ -10,22 +10,17 @@
     describe('arraySlice', function () {
         var arr = [utilx.privateUndefined, null, 1, 'a', 2, 'b', null, utilx.privateUndefined];
 
-        function someArgs() {
-            return utilx.returnArgs.apply(null, arr);
-        }
-
         it('should not throw an error in each case', function () {
-            expect(utilx.arraySlice(someArgs())).to.eql(utilx.arraySlice(arr));
-            expect(utilx.arraySlice(someArgs(), utilx.privateUndefined, utilx.privateUndefined))
-                .to.eql(utilx.arraySlice(arr, utilx.privateUndefined, utilx.privateUndefined));
-            expect(utilx.arraySlice(someArgs(), -1)).to.eql(utilx.arraySlice(arr, -1));
-            expect(utilx.arraySlice(someArgs(), -1).length).to.be(1);
-            expect(utilx.arraySlice(someArgs(), 0)).to.eql(utilx.arraySlice(arr, 0));
-            expect(utilx.arraySlice(someArgs(), 3)).to.eql(utilx.arraySlice(arr, 3));
-            expect(utilx.arraySlice(someArgs(), -1, 4)).to.eql(utilx.arraySlice(arr, -1, 4));
-            expect(utilx.arraySlice(someArgs(), -1, 4).length).to.be(0);
-            expect(utilx.arraySlice(someArgs(), 0, 4)).to.eql(utilx.arraySlice(arr, 0, 4));
-            expect(utilx.arraySlice(someArgs(), 3, 6)).to.eql(utilx.arraySlice(arr, 3, 6));
+            expect(utilx.arraySlice(arr)).to.eql(arr);
+            expect(utilx.arraySlice(arr, utilx.privateUndefined, utilx.privateUndefined)).to.eql(arr);
+            expect(utilx.arraySlice(arr, -1)).to.eql([utilx.privateUndefined]);
+            expect(utilx.arraySlice(arr, -1).length).to.be(1);
+            expect(utilx.arraySlice(arr, 0)).to.eql(arr);
+            expect(utilx.arraySlice(arr, 3)).to.eql(['a', 2, 'b', null, utilx.privateUndefined]);
+            expect(utilx.arraySlice(arr, -1, 4)).to.eql([]);
+            expect(utilx.arraySlice(arr, -1, 4).length).to.be(0);
+            expect(utilx.arraySlice(arr, 0, 4)).to.eql([utilx.privateUndefined, null, 1, 'a']);
+            expect(utilx.arraySlice(arr, 3, 6)).to.eql(['a', 2, 'b']);
         });
     });
 }());
