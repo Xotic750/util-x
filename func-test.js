99c8d11d51439517d3a28133fc827503e2f5d027
Xotic750 <xotic750@gmail.com>
2015-03-18 22:17:42 +0100

    Update tests

---
+++ func-test.js
@@ -0,0 +1,277 @@
+/*jslint browser: true, devel: true */
+
+(function () {
+    'use strict';
+
+    var $Object = Object,
+        hasOwn = $Object.prototype.hasOwnProperty,
+        toClass = $Object.prototype.toString,
+        $String = String,
+        isStrictMode,
+        hasCallBug,
+        print,
+        testItems,
+        methods;
+
+    function strictEqual(a, b) {
+        return a === b;
+    }
+
+    function isUndefined(inputArg) {
+        return strictEqual(typeof inputArg, 'undefined');
+    }
+
+    function isPrimitive(inputArg) {
+        var type = typeof inputArg;
+
+        return type === 'undefined' ||
+            inputArg === null ||
+            type === 'boolean' ||
+            type === 'string' ||
+            type === 'number';
+    }
+
+    function toInteger(inputArg) {
+        var number = +inputArg,
+            val = 0;
+
+        if (strictEqual(number, number)) {
+            if (!number || number === Infinity || number === -Infinity) {
+                val = number;
+            } else {
+                val = (number > 0 || -1) * Math.floor(Math.abs(number));
+            }
+        }
+
+        return val;
+    }
+
+    function stringify(inputArg) {
+        var val;
+
+        if (inputArg === null) {
+            val = 'null';
+        } else if (isUndefined(inputArg)) {
+            val = 'undefined';
+        } else {
+            val = $String(inputArg);
+        }
+
+        return val;
+    }
+
+    function checkObjectCoercible(inputArg) {
+        if (inputArg === null || isUndefined(inputArg)) {
+            throw new TypeError('Cannot convert argument to object: ' + stringify(inputArg));
+        }
+
+        return inputArg;
+    }
+
+    function toObject(inputArg) {
+        return $Object(checkObjectCoercible(inputArg));
+    }
+
+    function toLength(inputArg) {
+        return Math.min(Math.max(toInteger(inputArg), 0), Math.pow(2, 32) - 1);
+    }
+
+    function inPrototype(inputArg, property) {
+        if (isPrimitive(inputArg)) {
+            throw new TypeError('called on non-object: ' + stringify(inputArg));
+        }
+
+        var rtn;
+
+        if (!isPrimitive(inputArg.constructor) && !isPrimitive(inputArg.constructor.prototype)) {
+            rtn = hasOwn.call(inputArg.constructor.prototype, stringify(property));
+        } else {
+            rtn = false;
+        }
+
+        return rtn;
+    }
+
+    function isFunction(inputArg) {
+        return !isPrimitive(inputArg) &&
+            hasOwn.call(inputArg, 'length') &&
+            typeof inputArg.length === 'number' &&
+            inPrototype(inputArg, 'call') &&
+            inPrototype(inputArg, 'apply');
+    }
+
+    /*
+    function slice(array, start, end) {
+        var object = toObject(array),
+            length = toLength(object.length),
+            relativeStart = toInteger(start),
+            val = [],
+            next = 0,
+            relativeEnd,
+            finalEnd,
+            k;
+
+        if (relativeStart < 0) {
+            k = Math.Max(length + relativeStart, 0);
+        } else {
+            k = Math.min(relativeStart, length);
+        }
+
+        if (isUndefined(end)) {
+            relativeEnd = length;
+        } else {
+            relativeEnd = toInteger(end);
+        }
+
+        if (relativeEnd < 0) {
+            finalEnd = Math.max(length + relativeEnd, 0);
+        } else {
+            finalEnd = Math.min(relativeEnd, length);
+        }
+
+        val.length = Math.max(finalEnd - k, 0);
+        while (k < finalEnd) {
+            if (k in object) {
+                val[next] = object[k];
+            }
+
+            next += 1;
+            k += 1;
+        }
+
+        return val;
+    }
+    */
+
+    isStrictMode = (function () {
+        return !this;
+    }()) || false;
+
+    hasCallBug = (function () {
+        var returnThis = function () {
+            return this;
+        };
+
+        return !isStrictMode && typeof returnThis.call('foo') === 'string';
+    }()) || false;
+
+    function toObjectCallFix(inputArg) {
+        var object = inputArg,
+            type;
+
+        if (hasCallBug) {
+            type = typeof inputArg;
+            if (type === 'boolean' || type === 'number' || type === 'string') {
+                object = $Object(inputArg);
+            }
+        }
+
+        return object;
+    }
+
+    function forEach(arr, fn, thisArg) {
+        var object = toObject(arr),
+            length,
+            index;
+
+        if (!isFunction(fn)) {
+            throw new TypeError('Argument is not a function: ' + stringify(fn));
+        }
+
+        length = toLength(object.length);
+        if (length) {
+            thisArg = toObjectCallFix(thisArg);
+        }
+
+        for (index = 0; index < length; index += 1) {
+            if (index in object) {
+                fn.call(thisArg, object[index], index, object);
+            }
+        }
+    }
+
+    print = (function (out) {
+        return function print(mtd, itm, res) {
+            var row = document.createElement('div'),
+                method = document.createElement('div'),
+                item = document.createElement('div'),
+                result = document.createElement('div');
+
+            row.className = 'row';
+            method.className = 'method';
+            item.className = 'item';
+            result.className = 'result';
+            method.appendChild(document.createTextNode(stringify(mtd)));
+            item.appendChild(document.createTextNode(stringify(itm)));
+            result.appendChild(document.createTextNode(stringify(res)));
+            row.appendChild(method);
+            row.appendChild(item);
+            row.appendChild(result);
+            out.appendChild(row);
+        };
+    }(document.getElementById('out')));
+
+    methods = [{
+        name: 'typeof',
+        fn: function (inputArg) {
+            return typeof inputArg;
+        }
+    }, {
+        name: 'toString',
+        fn: function (inputArg) {
+            return toClass.call(inputArg);
+        }
+    }];
+
+    testItems = [{
+        name: 'Number',
+        value: Number
+    }, {
+        name: 'String',
+        value: String
+    }, {
+        name: 'Boolean',
+        value: Boolean
+    }, {
+        name: 'Array',
+        value: Array
+    }, {
+        name: 'Object',
+        value: Object
+    }, {
+        name: 'Function',
+        value: Function
+    }, {
+        name: 'RegExp',
+        value: RegExp
+    }, {
+        name: 'Date',
+        value: Date
+    }, {
+        name: 'isNaN',
+        value: isNaN
+    }, {
+        name: 'isFinite',
+        value: isFinite
+    }, {
+        name: 'setTimeout',
+        value: setTimeout
+    }, {
+        name: 'setInterval',
+        value: setInterval
+    }, {
+        name: 'alert',
+        value: alert
+    }, {
+        name: 'function () { return; }',
+        value: function () {
+            return;
+        }
+    }];
+
+    forEach(methods, function (method) {
+        forEach(testItems, function (testItem) {
+            print(method.name, testItem.name, method.fn(testItem.value));
+        });
+    });
+}());
