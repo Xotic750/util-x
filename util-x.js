5cb0244f20859242e5706c403c52a74c92c79b2a
Xotic750 <xotic750@gmail.com>
2015-03-30 21:59:30 +0200

    Update tests

--- util-x.js
+++ util-x.js
@@ -76,7 +76,7 @@
     toString, toStringTag, toUint, toUint16, toUint32, toUint8, toUpperCase,
     trim, trimLeft, trimRight, trimString, truncate, typeOf, unique, unshift,
     unwatch, value, valueOf, version, watch, wrapInChars, writable, wsStr, write,
-    ux
+    application, uxTest
 */
 
 /**
@@ -2837,10 +2837,14 @@
                 iframe.application = 'yes';
                 body.appendChild(iframe);
                 content = window.frames[$toLength(window.frames.length) - 1];
-                content.document.write('<script>parent.ux = { Array: Array };<\/script>');
-                supportsXFrameClass = $call(pOToString, new window.ux.Array(1, 2, 3)) === stringTagArray;
+                content.document.write('<script>parent.uxTest = { Array: Array };<\/script>');
+                supportsXFrameClass = $call(pOToString, new window.uxTest.Array(1, 2, 3)) === stringTagArray;
                 body.removeChild(iframe);
-                delete window.ux;
+                try {
+                    delete window.uxTest;
+                } catch (e) {
+                    window.uxTest = Undefined;
+                }
             };
 
             body = getBody();
